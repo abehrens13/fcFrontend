1b390fd8604c2192c877db27700cd36d8938558c
abehrens13 <andrey.behrens@openaqua.de>
2020-05-18 19:10:47 +0200

    4

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,8 +6,9 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { Button, ButtonGroup } from '@material-ui/core';
+import { PlaylistAddCheck, PlaylistPlay } from '@material-ui/icons/';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -33,33 +34,64 @@ const defaultInnerBoxProps = {
 	borderColor: 'text.primary',
 	m: 1,
 	border: 0,
-	borderRadius: 5
+	borderRadius: 5,
+	borderTop: 1,
+	borderBottom: 1
 
 };
 
 
 export default function Question() {
 	const classes = useStyles();
+	const [state, setState] = React.useState({
+		answerA: true,
+		answerB: false,
+		answerC: false,
+	});
+	const { answerA, answerB, answerC } = state;
+
+	const handleChange = (event) => {
+		//setState({ ...state, [event.target.name]: event.target.checked });
+	};
+
+
 	return (
 		<div className={classes.root}>
 			<FormControl component="fieldset" className={classes.formControl}>
 
 
 				<Box  {...defaultOuterBoxProps}>
-					<Box  {...defaultInnerBoxProps}>
+					<Box  {...defaultInnerBoxProps} borderTop={0} borderBottom={0}>
 						<h6>Question 1 of 30</h6>
-					</Box>
-					<Box {...defaultInnerBoxProps}>
 						<p>All around me are familiar faces Worn out places, worn out faces Bright and early for the daily races Going nowhere, going nowhere. Their tears are filling up their glasses No expression, no expression Hide my head I wanna drown my sorrow No tomorrow, no tomorrow. And I find it kind of funny I find it kind of sad</p>
-					</Box>
-					<Box {...defaultInnerBoxProps}>
 						<p>(choose the best)</p>
 					</Box>
-					<Box {...defaultInnerBoxProps}>
-						<p>A lot of answers</p>
+					<Box {...defaultInnerBoxProps} >
+						<FormGroup>
+							<FormControlLabel
+								control={<Checkbox checked={answerA} onChange={handleChange} name="gilad" />}
+								label="Rot ist was anderes als Grün"
+							/>
+							<FormControlLabel
+								control={<Checkbox checked={answerB} onChange={handleChange} name="jason" />}
+								label="Blau sieht nur selten aus wie Rot"
+							/>
+							<FormControlLabel
+								control={<Checkbox checked={answerC} onChange={handleChange} name="antoine" />}
+								label="Wenn man Rot und Grün mischt, entsteht ein schmutziges Braun."
+							/>
+						</FormGroup>
+
 					</Box>
-					<Box {...defaultInnerBoxProps}>
-						<p>Explanation</p>
+					<Box {...defaultInnerBoxProps} borderTop={0} borderBottom={0}>
+						<ButtonGroup>
+							<Button variant="contained" color="secondary" startIcon={<PlaylistAddCheck />}>Check</Button>
+							<Button variant="contained" color="primary" startIcon={<PlaylistPlay />}>Next</Button>
+						</ButtonGroup>
+
+					</Box>
+					<Box {...defaultInnerBoxProps} >
+						<p>I used to play this song every day while I was battling cancer. Two years ago, I went into the storm with little hope of coming out. Thinking about My Three Beautiful Children I was going to leave behind... But I’m still here and enjoying every moment of my life. </p>
 					</Box>
 
 				</Box>
@@ -71,42 +103,17 @@ export default function Question() {
 
 export function Question2() {
 	const classes = useStyles();
-	const [state, setState] = React.useState({
-		gilad: true,
-		jason: false,
-		antoine: false,
-	});
 
-	const handleChange = (event) => {
-		setState({ ...state, [event.target.name]: event.target.checked });
-	};
 
-	const { gilad, jason, antoine } = state;
+
+
 
 	return (
 		<div className={classes.root}>
 
-			<Box display="flex" justifyContent="center">
-				<FormLabel component="legend">Question 1 of 30</FormLabel>
-				<p>What is what us drive the most?</p>
-
-			</Box>
+			<FormLabel component="legend">Question 1 of 30</FormLabel>
 
 			<FormControl component="fieldset" className={classes.formControl}>
-				<FormGroup>
-					<FormControlLabel
-						control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
-						label="Gilad Gray"
-					/>
-					<FormControlLabel
-						control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
-						label="Jason Killian"
-					/>
-					<FormControlLabel
-						control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
-						label="Antoine Llorca"
-					/>
-				</FormGroup>
 				<FormHelperText>(choose the best)</FormHelperText>
 			</FormControl>
 
